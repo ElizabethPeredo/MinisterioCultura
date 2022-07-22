ed6cf77ea62e1960dac8fdee266aa550451c9708
ElizabethPeredo <61327189+ElizabethPeredo@users.noreply.github.com>
2022-07-22 03:18:29 -0500

    Fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
 import Navbar from '../src/components/Header/Navbar/Navbar';
 import Footer from '../src/components/Footer/Footer';
-import Modal from '../src/components/Modal/Modal';
-//import css from '../src/assets/css/global.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'; //importing font awesome css
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
-import Script from 'next/script'
 import '../src/assets/css/global.css'
 import 'tailwindcss/tailwind.css';
 
