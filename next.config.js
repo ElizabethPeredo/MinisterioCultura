77bdf18a14fc3efa58cc9c995208c6f3a5e5c0f9
ElizabethPeredo <61327189+ElizabethPeredo@users.noreply.github.com>
2022-07-25 15:45:10 -0500

    Fix 6

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  webpack(config, { dev }) {
-    // modify it!
-    return config;
-  },
-  trailingSlash: true/flase,
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+  }
+  return nextConfig
 }
